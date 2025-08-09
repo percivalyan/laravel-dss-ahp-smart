c9dc84d5ca3611e3e36be0c6c50d980f64c3ca76
percivalyan <yoryan8888@gmail.com>
2025-08-09 11:42:08 +0700

    feat: user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'Admin',
+                'email' => 'admin@mail.com',
+                'password' => Hash::make('admin123'),
+                'role' => 'admin',
+                'email_verified_at' => Carbon::now(),
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'User',
+                'email' => 'user@mail.com',
+                'password' => Hash::make('user123'),
+                'role' => 'user',
+                'email_verified_at' => Carbon::now(),
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
