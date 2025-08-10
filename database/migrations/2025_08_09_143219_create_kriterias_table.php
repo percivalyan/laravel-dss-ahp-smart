06a42f1caa606da985a66f31821640ab6db83ba6
percivalyan <yoryan8888@gmail.com>
2025-08-10 21:30:20 +0700

    add: model kriteria & sub kriteria

---
+++ database/migrations/2025_08_09_143219_create_kriterias_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kriterias', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode_kriteria')->unique();
+            $table->string('nama_kriteria')->unique();
+            $table->string('jenis_kriteria')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kriterias');
+    }
+};
